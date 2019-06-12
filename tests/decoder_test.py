0466e425b5c1ef684c600a0f5e426cd11e878e7c
Johan McQuillan <johangmcquillan@gmail.com>
2019-06-12 22:55:49 +0100

    update test to  include flag options

--- tests/decoder_test.py
+++ tests/decoder_test.py
@@ -49,10 +49,27 @@ class TestDecoder(unittest.TestCase):
         self.assertEqual(b'', p.stderr)
 
 
-def assertExecutesSuccessfully(test: TestDecoder, path: str, stdin=None) -> subprocess.CompletedProcess:
-        p = subprocess.run([path], capture_output=True, input=stdin)
-        test.assertEqual(
-            0, p.returncode,
-            msg=f'process failed with code {p.returncode};\nstderr: {p.stderr}'
-        )
-        return p
+def assertExecutesSuccessfully(
+        test: TestDecoder, path: str, stdin=None, orig_name=True, enums=False, defaults=False, indent='',
+) -> subprocess.CompletedProcess:
+    args = []
+    if orig_name:
+        args.append('-o')
+    if enums:
+        args.append('-e')
+    if indent != '':
+        args.append('-i=' + indent)
+    if defaults:
+        args.append('-d')
+
+    cmd = [path]
+    if len(args) > 0:
+        cmd.append(' '.join(args))
+
+    p = subprocess.run(cmd, capture_output=True, input=stdin)
+    test.assertEqual(
+        0, p.returncode,
+        msg=f'process failed with code {p.returncode};\nstderr: {p.stderr}'
+    )
+
+    return p
